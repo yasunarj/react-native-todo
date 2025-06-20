f8002fce5d1ff804f60af9464be99b19db4fbdd5
nari <yasunarj@gmail.com>
2025-06-20 19:03:02 +0900

    Add: todoListの削除機能の実装

--- app/(tabs)/todo.tsx
+++ app/(tabs)/todo.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { SwipeListView } from "react-native-swipe-list-view";
+
 import {
   Text,
   View,
@@ -6,6 +8,7 @@ import {
   Button,
   FlatList,
   StyleSheet,
+  TouchableOpacity,
 } from "react-native";
 
 const TodoScreen = () => {
@@ -18,6 +21,12 @@ const TodoScreen = () => {
       setTask("");
     }
   };
+
+  const handleDelete = (index: number) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  }
 
   return (
     <View style={styles.container}>
@@ -33,11 +42,27 @@ const TodoScreen = () => {
         <Button title="追加" onPress={addTask} />
       </View>
 
-      <FlatList
+      <SwipeListView
+        data={tasks.map((item, index) => ({ key: index.toString(), value: item }))}
+        renderItem={({ item }) => (
+          <View style={styles.rowFront}>
+            <Text>・{item.value}</Text>
+          </View>
+        )}
+        renderHiddenItem={({ index }) => (
+          <View style={styles.rowBack}>
+            <TouchableOpacity onPress={() => handleDelete(index)} style={styles.deleteButton}>
+              <Text style={styles.deleteText}>削除</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        rightOpenValue={-75}
+      />
+      {/* <FlatList
         data={tasks}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => <Text style={styles.taskItem}>・{item}</Text>}
-      />
+      /> */}
     </View>
   );
 };
@@ -66,10 +91,33 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
   },
-  taskItem: {
-    fontSize: 18,
-    marginVertical: 5,
+  rowFront: {
+    backgroundColor: '#f2f2f2',
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 5,
   },
+  rowBack: {
+    alignItems: 'flex-end',
+    backgroundColor: 'red',
+    flex: 1,
+    justifyContent: 'center',
+    paddingRight: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  deleteButton: {
+    height: '100%',
+    justifyContent: 'center',
+  },
+  deleteText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  }
+  // taskItem: {
+  //   fontSize: 18,
+  //   marginVertical: 5,
+  // },
 });
 
 export default TodoScreen;
